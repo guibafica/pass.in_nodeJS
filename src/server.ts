bf4c81f99002ccfffa0a6a5920f2996e59bccdb5
Guilherme Bafica <gb.engcomp@hotmail.com>
2024-05-09 10:21:41 -0400

    Configure routes

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,9 @@
-console.log("Hello world!");
+import fastify from "fastify";
+
+const app = fastify();
+
+app.get("/", () => "Hello world");
+
+app.listen({ port: 3333 }).then(() => {
+  console.log("🚀 HTTP server running! 🚀");
+});
